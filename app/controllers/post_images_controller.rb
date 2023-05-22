f1ee7e5f80a2e89b3c1d8881798261bcfe6469f1
yada-kent <42689607+yada-kent@users.noreply.github.com>
2023-05-22 01:26:03 +0000

    バリデーションの設定

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,24 +2,27 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   #投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     if @post_image.destroy
@@ -28,9 +31,9 @@ class PostImagesController < ApplicationController
   end
   # 投稿データのストロングパラメータ
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
